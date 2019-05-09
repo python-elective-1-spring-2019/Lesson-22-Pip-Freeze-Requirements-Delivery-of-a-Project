e69bddb9cf5aa456d5cdb1dd58763005123218c6
clbokea <clbo@kea.dk>
2019-05-09 10:00:37 +0200

    init

---
+++ exercises/pip_freeze.py
@@ -0,0 +1,6 @@
+"""
+    Exercise: Pip freeze
+    Create a simple application that make use of a module installed through pip (pip install <<module>>).
+    Then craete a repository on github and push you new aaplication to this repository. 
+    The Repository should have a readme.md file with a description on how to install and use the application.  
+"""
