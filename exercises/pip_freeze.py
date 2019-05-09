f1f4b19f68d14a6281c0dec743470b00479d09fe
clbokea <clbo@kea.dk>
2019-05-09 17:12:32 +0200

    exercises real

--- exercises/pip_freeze.py
+++ exercises/pip_freeze.py
@@ -1,6 +1,15 @@
 """
-    Exercise: Pip freeze
-    Create a simple application that make use of a module installed through pip (pip install <<module>>).
-    Then craete a repository on github and push you new aaplication to this repository. 
-    The Repository should have a readme.md file with a description on how to install and use the application.  
+    1. 
+    Create a project (script file) with some simple code.
+    Install a 3. party module using pip.
+    Create a requirements.txt file
+    Create a Readme.md file and put a description of the script and a description on how to install the project.
+    Push the project to github.
+    
+    2. 
+    Create a new virtual enviroment.
+    Install the project from github on your computer. 
+    Install the requirements
+    Run the program.
+
 """
